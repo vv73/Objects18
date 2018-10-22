d947f4a34a4e49e50365e4ee55416ce0e6f89894
vv <u0NEEp9v>
2018-10-22 20:13:24 +0300

    Hero constructors & functions

--- app/src/main/java/com/vv/objects18/DrawView.java
+++ app/src/main/java/com/vv/objects18/DrawView.java
@@ -5,25 +5,28 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class DrawView extends View {
     Hero hero;
+    Hero hero2;
     public DrawView(Context context) {
         super(context);
         setBackgroundColor(Color.GRAY);
         Bitmap pic = BitmapFactory
                 .decodeResource(getResources(),
                         R.drawable.team_lead);
-        hero = new Hero();
-        hero.x = 10;
-        hero.y = 25;
-        hero.pic = pic;
-        hero.name = "Jonny";
+        hero  = new Hero( "Jonny", pic);
+        hero2 = new Hero( "Mike", pic);
     }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        hero.go();
         hero.draw(canvas);
+        hero2.go();
+        hero2.draw(canvas);
+        invalidate();
     }
 }
--- app/src/main/java/com/vv/objects18/Hero.java
+++ app/src/main/java/com/vv/objects18/Hero.java
@@ -8,16 +8,44 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
+import java.util.Random;
+
 public class Hero {
-    int x, y;
+    float x, y;
+    float vx, vy;
     String name;
     Bitmap pic;
 
+    void go(){
+        x += vx;
+        y += vy;
+        if (x < 0) x = 1080;
+        if (x > 1080) x = 0;
+        if (y > 1920) y = 0;
+        if (y < 0) y = 1920;
+    }
+
+    Hero(float x, float y,  String name, Bitmap pic, float vx, float vy){
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.pic = pic;
+        this.vx = vx;
+        this.vy = vy;
+    }
+    Hero(String name, Bitmap pic){
+        this.name = name;
+        this.pic = pic;
+        Random r = new Random();
+        this.x = r.nextInt(800);
+        this.y = r.nextInt(1000);
+        this.vx = r.nextFloat() * 4 - 2;
+        this.vy = r.nextFloat() * 4 - 2;
+    }
+
     void draw(Canvas canvas){
-         Rect from = new Rect(0, 0,
-                 pic.getWidth(), pic.getHeight());
-         Rect to = new Rect(x, y, x + 100, y + 200);
-         canvas.drawBitmap(pic, from, to, null);
+         RectF to = new RectF(x, y, x + 100, y + 200);
+         canvas.drawBitmap(pic, null, to, null);
 
     }
 }
